b11192e8987c3833e918b3af1719fa6d78a62f31
BenBiggen <benjamin.storesund@gmail.com>
2026-09-11 10:21:25 +0200

    11 september
    Gjort en god del arbeid på rpg spill

--- rpgspill.py
+++ rpgspill.py
@@ -3,28 +3,54 @@ import random
 roomCommonLoot = ["Vanlig Sverd", "Vanlig Bue", "Vanlig Spyd"]
 roomRareLoot = ["Sjeldent Sverd", "Sjelden Bue", "Sjeldent spyd"]
 roomLegendaryLoot = ["Legendarisk Flamme Sverd", "Legendarisk Skarpt Spyd", "Legendarisk Sterk Bue"]
-    
+enemies = ["Farlig Gnom", "Kjempe Stor Kjempe"]
+heldWeapon = "a"
+
+
+
 def choiceYN():
     gyldigInput = False
 
     while not gyldigInput:
-        yesno = input("[Y, N]:")
+        yesno = input("[Y, N]: ")
         positiveAlternativer = ["Y","y","yes", "Ja"]
         negativeAlternativer = ["N","n","No","Nei"]
 
         if yesno in positiveAlternativer:
-            print("positivt innstilt")
             gyldigInput = True
+            yesno = "Y"
         elif yesno in negativeAlternativer:
-            print("negativ")
             gyldigInput = True
+            yesno="N"
         else:
             gyldigInput = False
+            print("Invalid Input (Y, N)")
+
+    return yesno
+
+def weaponEquip(weapon):
+    if heldWeapon != weapon:
+        heldWeapon = weapon
+
+def damageEnemy():
+    if heldWeapon in roomCommonLoot:
+        dmgMultiplier = 1
+    elif heldWeapon in roomRareLoot:
+        dmgMultiplier = 2
+    elif heldWeapon in roomLegendaryLoot:
+        dmgMultiplier = 4
+
+    damage = random.randint(1, 3)*dmgMultiplier
         
 
 def lootEncounter(lootTable):
-    return random.choice(lootTable)
+    lootFind = random.choice(lootTable)
+    print (f"Du trakk {lootFind}, vil du bytte ut våpenet ditt?")
+    if choiceYN() == "Y":
+        weaponEquip(lootFind)
+        return lootFind
+    
 
 #def roomEncounter():
 
-choiceYN()
+print(lootEncounter(roomCommonLoot))
